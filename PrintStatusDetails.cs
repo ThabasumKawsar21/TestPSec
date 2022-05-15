8f9a8ea5e371f08bbbc1a8d0439532fdee0de539
ThabasumKawsar21 <97147466+ThabasumKawsar21@users.noreply.github.com>
2022-05-15 17:27:39 +0530

    Create PrintStatusDetails.cs

---
+++ PrintStatusDetails.cs
@@ -0,0 +1,44 @@
+
+namespace VMSBusinessEntity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// The PrintStatusDetails class
+    /// </summary>    
+    public class PrintStatusDetails
+    {
+        /// <summary>
+        /// Gets or sets the ContractorId property
+        /// </summary>        
+        public string ContractorId { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the PrintStatus property
+        /// </summary>        
+        public string PrintStatus { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the UpdatedBy property
+        /// </summary>        
+        public string UpdatedBy { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the LocationName property
+        /// </summary>        
+        public string LocationName { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the PrintedDate property
+        /// </summary>        
+        public string PrintedDate { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the IDGeneratedDate property
+        /// </summary>        
+        public string IDGeneratedDate { get; set; }
+    }
+}
