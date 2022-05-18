1d7f163bb883b56e2d3315e3414ca8f78859d822
ThabasumKawsar21 <97147466+ThabasumKawsar21@users.noreply.github.com>
2022-05-18 16:17:36 +0530

    Create 1CAAPReportsPage.aspx.cs

---
+++ 1CAAPReportsPage.aspx.cs
@@ -0,0 +1,25 @@
+
+namespace VMSDev
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+    /// <summary>
+    /// Report page
+    /// </summary>
+    public partial class _1CAAPReportsPage : System.Web.UI.Page
+    {
+        /// <summary>
+        /// The Page_Load method
+        /// </summary>
+        /// <param name="sender">The sender parameter</param>
+        /// <param name="e">The e parameter</param>        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
