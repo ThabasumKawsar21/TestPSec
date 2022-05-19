42995668dabe30d4c89ccd58d361fe87c45298b3
ThabasumKawsar21 <97147466+ThabasumKawsar21@users.noreply.github.com>
2022-05-19 15:39:22 +0530

    Create HostWP.aspx.cs

---
+++ HostWP.aspx.cs
@@ -0,0 +1,25 @@
+
+namespace VMSDev
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+    /// <summary>
+    ///   for host WP
+    /// </summary>
+    public partial class HostWP : System.Web.UI.Page
+    {
+        /// <summary>
+        /// The Page_Load method
+        /// </summary>
+        /// <param name="sender">The sender parameter</param>
+        /// <param name="e">The e parameter</param>        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
