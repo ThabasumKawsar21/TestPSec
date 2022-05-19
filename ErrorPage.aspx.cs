7092bbe56e0043c3cc06312289e75512f80a80db
ThabasumKawsar21 <97147466+ThabasumKawsar21@users.noreply.github.com>
2022-05-19 14:47:40 +0530

    Create ErrorPage.aspx.cs

---
+++ ErrorPage.aspx.cs
@@ -0,0 +1,34 @@
+
+namespace VMSDev
+{
+    using System;
+    using System.Collections;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Security;
+    using System.Web.UI;
+    using System.Web.UI.HtmlControls;
+    using System.Web.UI.WebControls;
+    using System.Web.UI.WebControls.WebParts;
+    using System.Xml.Linq;
+    using VMSDev;
+
+    /// <summary>
+    /// Error page partial class
+    /// </summary>
+    public partial class ErrorPage : System.Web.UI.Page
+    {
+        #region pageload
+        /// <summary>
+        /// Page load event
+        /// </summary>
+        /// <param name="sender">sender parameter</param>
+        /// <param name="e">The e parameter</param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        #endregion
+    }
+}
