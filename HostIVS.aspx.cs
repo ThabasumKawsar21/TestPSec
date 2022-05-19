dca7a1dd15e13ab7cff2740479a700ce57d33e16
ThabasumKawsar21 <97147466+ThabasumKawsar21@users.noreply.github.com>
2022-05-19 15:29:46 +0530

    Create HostIVS.aspx.cs

---
+++ HostIVS.aspx.cs
@@ -0,0 +1,32 @@
+//-----------------------------------------------------------------------
+// <copyright file="HostIVS.aspx.cs" company="MyCompanyName">
+//     Copyright (c) MyCompanyName. All rights reserved. 
+// </copyright>
+// <summary>
+// This file contains HostIVS class.
+// </summary>
+//-----------------------------------------------------------------------
+namespace VMSDev
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+    /// <summary>
+    /// Host IVS partial class
+    /// </summary>
+    public partial class HostIVS : System.Web.UI.Page
+    {
+        /// <summary>
+        /// The Page_Load method
+        /// </summary>
+        /// <param name="sender">The sender parameter</param>
+        /// <param name="e">The e parameter</param>        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
