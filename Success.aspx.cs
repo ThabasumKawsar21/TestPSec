9ed65f4b093554004d0e245f38dfc54166aa0700
ThabasumKawsar21 <97147466+ThabasumKawsar21@users.noreply.github.com>
2022-05-23 17:18:37 +0530

    Create Success.aspx.cs

---
+++ Success.aspx.cs
@@ -0,0 +1,32 @@
+
+namespace VMSDev
+{
+    using System;
+    using System.Collections;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Security;
+    using System.Web.UI;
+    using System.Web.UI.HtmlControls;
+    using System.Web.UI.WebControls;
+    using System.Web.UI.WebControls.WebParts;
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// Partial class Success
+    /// </summary>
+    public partial class Success : System.Web.UI.Page
+    {
+        /// <summary>
+        /// The Page_Load method
+        /// </summary>
+        /// <param name="sender">The sender parameter</param>
+        /// <param name="e">The e parameter</param>        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           // lblMessage.Text = "Submitted Successfully";
+        }
+    }
+}
