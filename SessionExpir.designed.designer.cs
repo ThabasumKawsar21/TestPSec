2d61d2ee040309b6b103d9a22f184ee286fdfd43
ThabasumKawsar21 <97147466+ThabasumKawsar21@users.noreply.github.com>
2022-05-23 17:10:46 +0530

    Create SessionExpir.designed.designer.cs

---
+++ SessionExpir.designed.designer.cs
@@ -0,0 +1,44 @@
+
+
+namespace VMSDev {
+    
+    
+    public partial class SessionExpired {
+        
+        /// <summary>
+        /// Head1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
+        
+        /// <summary>
+        /// frmsessionexpired control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm frmsessionexpired;
+        
+        /// <summary>
+        /// lblApplicationName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblApplicationName;
+        
+        /// <summary>
+        /// Loginurl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor Loginurl;
+    }
+}
